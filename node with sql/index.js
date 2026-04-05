46052c7a6262b7d93174c298eba6f02a5778222d
Navaneeth <tammineedinavaneeth2@gmail.com>
2026-04-06 00:26:49 +0530

    added node with sql

---
+++ node with sql/index.js
@@ -0,0 +1,30 @@
+const { faker } = require('@faker-js/faker');
+const mysql=require("mysql2");
+
+const connection =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password:"sqlkabaap"
+});
+
+try{
+    connection.query("Show Tables",(err,result)=>{
+        if (err) throw err;
+        console.log(result);
+
+    })
+}catch(err){
+    console.log(err);
+}
+connection.end();
+
+let  getRandomUser=()=> {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
+
